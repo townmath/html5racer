c01492f5f5863a1c520c25853430eb5354a0f4f1
Greg Elin <greg@fotonotes.net>
2014-03-13 22:52:15 -0400

    Basic self driving around bezier curves

--- racer.js
+++ racer.js
@@ -12,12 +12,15 @@ var canvas   = document.getElementById('canvas'),
 	elPY     = document.getElementById('py'),
 	elPE     = document.getElementById('pe'),
 	elPS     = document.getElementById('ps'),
-	elSG	 = document.getElementById('sg')
+	elSG	 = document.getElementById('sg'),
+	elTC	 = document.getElementById('tc')
 
 ;
 
 track.src = "track2.png";
 trackHit.src = "track2-hit.png";
+
+
 
 // collision
 var hit = new HitMap(trackHit);
@@ -95,6 +98,47 @@ function draw (car) {
 	context.drawImage(track, 0, 0);
 	drawRotatedImage(car.img, car.x, car.y, car.rotation);
 }
+function drawCurve() {
+	context.lineWidth = 2;
+	context.strokeStyle = "#bbb";
+	context.beginPath();
+	sx = 0;
+	sx = 40;
+	sy = 200;
+
+	context.moveTo(309, 592);
+	context.bezierCurveTo(124, 598, 123, 351, 306, 362);
+	context.stroke();
+}
+function stepT(t) {
+	elTC.innerHTML = Math.floor(t*10000)/10000;
+	t += speed;
+	return t;
+}
+function drawCurve2(t, p0, p1, p2, p3) {
+	// p0, p1, p2, p3 define points for bezier curve
+	var at = 1 - t;
+	var green1x = p0.x * t + p1.x * at;
+	var green1y = p0.y * t + p1.y * at;
+	var green2x = p1.x * t + p2.x * at;
+	var green2y = p1.y * t + p2.y * at;
+	var green3x = p2.x * t + p3.x * at;
+	var green3y = p2.y * t + p3.y * at;
+	var blue1x = green1x * t + green2x * at;
+	var blue1y = green1y * t + green2y * at;
+	var blue2x = green2x * t + green3x * at;
+	var blue2y = green2y * t + green3y * at;
+	var finalx = blue1x * t + blue2x * at;
+	var finaly = blue1y * t + blue2y * at;
+
+	// context.clearRect(0, 0, canvas.width, canvas.height);
+	context.beginPath();
+	context.arc(finalx, finaly, 10, 0, 2 * Math.PI, false);
+	context.fillStyle = 'red';
+	context.fill();
+	context.closePath();	
+
+}
 
 // Keyboard event listeners
 $(window).keydown(function(e){
@@ -113,9 +157,60 @@ $(window).keyup(function(e){
 function frame () {
 	step(player);
 	draw(player);
+	drawCurve();
+	t = stepT(t,speed);
+	
+	if ( player.segment == 1) {
+		var p0 = {"x": 268, "y": 362};
+		var p1 = {"x": 268, "y": 362};
+		var p2 = {"x": 700, "y": 362};
+		var p3 = {"x": 700, "y": 362};
+		if (t > 1) {
+			t = 0;
+			player.x = 267;
+		}
+		drawCurve2(t, p0, p1, p2, p3);
+	}
+	if ( player.segment == 2) {
+		var p0 = {"x": 268, "y": 592};
+		var p1 = {"x": 124, "y": 598};
+		var p2 = {"x": 123, "y": 351};
+		var p3 = {"x": 268, "y": 362};
+		if (t > 1) {
+			t = 0;
+			player.x = 280;
+			player.y = 598;
+		}
+		drawCurve2(t, p0, p1, p2, p3);
+	}
+	if ( player.segment == 3) {
+		var p0 = {"x": 700, "y": 598};
+		var p1 = {"x": 700, "y": 598};
+		var p2 = {"x": 268, "y": 598};
+		var p3 = {"x": 268, "y": 598};
+		if (t > 1) {
+			t = 0;
+			player.x = 800;
+		}
+		drawCurve2(t, p0, p1, p2, p3);
+	}
+	if ( player.segment == 4) {
+		var p3 = {"x": 309+400, "y": 592};
+		var p2 = {"x": 124+800, "y": 598};
+		var p1 = {"x": 123+800, "y": 351};
+		var p0 = {"x": 306+400, "y": 362};
+		if (t > 1) {
+			t = 0;
+			player.x = 699;
+			player.y = 362;
+		}
+		drawCurve2(t, p0, p1, p2, p3);
+	}
+
 	window.requestAnimationFrame(frame);
 }
-
+t = 0;
+speed = 0.004;
 frame();
 
 
