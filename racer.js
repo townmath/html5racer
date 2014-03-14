6d23b844387c52e9e17c8902a622d0ce6dc6c8d3
Greg Elin <greg@fotonotes.net>
2014-03-14 09:30:49 -0400

    Successful self-driving. A little bumpy on transitions

--- racer.js
+++ racer.js
@@ -20,8 +20,6 @@ var canvas   = document.getElementById('canvas'),
 track.src = "track2.png";
 trackHit.src = "track2-hit.png";
 
-
-
 // collision
 var hit = new HitMap(trackHit);
 
@@ -115,7 +113,7 @@ function stepT(t) {
 	t += speed;
 	return t;
 }
-function drawCurve2(t, p0, p1, p2, p3) {
+function drawCurve2(t, p0, p1, p2, p3, player) {
 	// p0, p1, p2, p3 define points for bezier curve
 	var at = 1 - t;
 	var green1x = p0.x * t + p1.x * at;
@@ -132,11 +130,13 @@ function drawCurve2(t, p0, p1, p2, p3) {
 	var finaly = blue1y * t + blue2y * at;
 
 	// context.clearRect(0, 0, canvas.width, canvas.height);
-	context.beginPath();
-	context.arc(finalx, finaly, 10, 0, 2 * Math.PI, false);
-	context.fillStyle = 'red';
-	context.fill();
-	context.closePath();	
+	// context.beginPath();
+	// context.arc(finalx, finaly, 10, 0, 2 * Math.PI, false);
+	// context.fillStyle = 'red';
+	// context.fill();
+	// context.closePath();
+	player.x = 	finalx;
+	player.y = finaly;
 
 }
 
@@ -157,60 +157,71 @@ $(window).keyup(function(e){
 function frame () {
 	step(player);
 	draw(player);
-	drawCurve();
+	// drawCurve();
 	t = stepT(t,speed);
+
+	if ( player.segment == 5 ) player.segment = 1;
 	
-	if ( player.segment == 1) {
+	if ( player.segment == 1 ) {
 		var p0 = {"x": 268, "y": 362};
 		var p1 = {"x": 268, "y": 362};
 		var p2 = {"x": 700, "y": 362};
 		var p3 = {"x": 700, "y": 362};
-		if (t > 1) {
+		drawCurve2(t, p0, p1, p2, p3, player);
+		if (t >= 0.98) {
 			t = 0;
-			player.x = 267;
+			player.segment = 2;
 		}
-		drawCurve2(t, p0, p1, p2, p3);
 	}
-	if ( player.segment == 2) {
+	if ( player.segment == 2 ) {
 		var p0 = {"x": 268, "y": 592};
 		var p1 = {"x": 124, "y": 598};
 		var p2 = {"x": 123, "y": 351};
 		var p3 = {"x": 268, "y": 362};
-		if (t > 1) {
+		drawCurve2(t, p0, p1, p2, p3, player);
+		if (t >= 0.98) {
 			t = 0;
-			player.x = 280;
-			player.y = 598;
+			player.segment = 3;
 		}
-		drawCurve2(t, p0, p1, p2, p3);
 	}
-	if ( player.segment == 3) {
+	if ( player.segment == 3 ) {
 		var p0 = {"x": 700, "y": 598};
 		var p1 = {"x": 700, "y": 598};
 		var p2 = {"x": 268, "y": 598};
 		var p3 = {"x": 268, "y": 598};
-		if (t > 1) {
+		drawCurve2(t, p0, p1, p2, p3, player);
+		if (t >= 0.9) {
 			t = 0;
-			player.x = 800;
+			player.segment = 4;
 		}
-		drawCurve2(t, p0, p1, p2, p3);
 	}
-	if ( player.segment == 4) {
+	if ( player.segment == 4 ) {
 		var p3 = {"x": 309+400, "y": 592};
 		var p2 = {"x": 124+800, "y": 598};
 		var p1 = {"x": 123+800, "y": 351};
 		var p0 = {"x": 306+400, "y": 362};
-		if (t > 1) {
+		drawCurve2(t, p0, p1, p2, p3, player);
+		if (t >= 0.9) {
 			t = 0;
-			player.x = 699;
-			player.y = 362;
+			player.segment = 5;
 		}
-		drawCurve2(t, p0, p1, p2, p3);
 	}
-
+	if ( player.segment == 5 ) {
+		var p0 = {"x": 268, "y": 362};
+		var p1 = {"x": 268, "y": 362};
+		var p2 = {"x": 700, "y": 362};
+		var p3 = {"x": 700, "y": 362};
+		drawCurve2(t, p0, p1, p2, p3, player);
+		if (t >= 0.98) {
+			t = 0;
+			player.segment = 2;
+		}
+	}
+	
 	window.requestAnimationFrame(frame);
 }
 t = 0;
-speed = 0.004;
+speed = 0.009;
 frame();
 
 
